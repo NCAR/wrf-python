5887669d5b3d5cc315718841f2d34d6379f9660d
Bill Ladwig <ladwig@ucar.edu>
2016-05-23 16:46:04 -0600

    changed package path

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ import numpy.distutils.core
  
 ext1 = numpy.distutils.core.Extension(
     name = "wrf._wrfext",
-    sources = ["src/python/wrf/wrfext.f90",
-               "src/python/wrf/wrfext.pyf"]
+    sources = ["src/wrf/wrfext.f90",
+               "src/wrf/wrfext.pyf"]
     )
 
 ext2 = numpy.distutils.core.Extension(
     name = "wrf._wrfcape",
-    sources = ["src/python/wrf/wrfcape.f90",
-               "src/python/wrf/wrfcape.pyf"]
+    sources = ["src/wrf/wrfcape.f90",
+               "src/wrf/wrfcape.pyf"]
     )
 
 numpy.distutils.core.setup( 
     name = "wrf",
     version = "0.0.1",        
-    packages = setuptools.find_packages("src/python"),   
+    packages = setuptools.find_packages("src"),   
     ext_modules = [ext1,ext2],
-    package_dir={"":"src/python"},
+    package_dir={"":"src"},
     #namespace_packages=["wrf"],
     scripts=[],
 )  
