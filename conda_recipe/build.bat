e3a4cf9d481ff6704af27492ca1a3bbaf8b2ddbb
Bill Ladwig <ladwig@ucar.edu>
2017-01-06 16:19:30 -0700

    Added a build bat for Windows

---
+++ conda_recipe/build.bat
@@ -0,0 +1,1 @@
+pip install --global-option build --global-option --compiler=mingw32 --global-option --fcompiler=gnu95
--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -4,10 +4,8 @@ if [ `uname` == Darwin ]; then
     LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
 fi
 
-if [`uname` == Darwin] || [`uname` == Linux]; then
-    pip install .
-else
-    pip install --global-option build --global-option --compiler=mingw32 --global-option --fcompiler=gnu95
-fi
+
+pip install .
+
 
 
