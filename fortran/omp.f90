78f34b4e693a47d885861973cd0e4126ecc6f8db
Bill Ladwig <ladwig@ucar.edu>
2017-12-15 15:21:38 -0700

    Cleaned up the default implementation of omp.f90 and the pyf file.

--- fortran/omp.f90
+++ fortran/omp.f90
@@ -1,9 +1,5 @@
-# 1 "ompgen.F90"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 1 "ompgen.F90"
+
 MODULE omp_constants
-# 52 "ompgen.F90"
     INTEGER, PARAMETER :: fomp_sched_kind = 4
     INTEGER, PARAMETER :: fomp_lock_kind = 4
     INTEGER, PARAMETER :: fomp_nest_lock_kind = 8
@@ -11,15 +7,10 @@ MODULE omp_constants
     INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_dynamic = 2
     INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_guided = 3
     INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_auto = 4
-
-
 END MODULE omp_constants
 
 
 SUBROUTINE fomp_set_num_threads(num_threads)
-
-
-
 
     IMPLICIT NONE
 
@@ -27,17 +18,10 @@ SUBROUTINE fomp_set_num_threads(num_threads)
 
     INTEGER, INTENT(IN) :: num_threads
 
-
-
-
-
 END SUBROUTINE fomp_set_num_threads
 
 
 FUNCTION fomp_get_num_threads()
-
-
-
 
     IMPLICIT NONE
 
@@ -45,19 +29,12 @@ FUNCTION fomp_get_num_threads()
 
     INTEGER :: fomp_get_num_threads
 
-
-
-
     fomp_get_num_threads = -1
-
 
 END FUNCTION fomp_get_num_threads
 
 
 FUNCTION fomp_get_max_threads()
-
-
-
 
     IMPLICIT NONE
 
@@ -65,19 +42,12 @@ FUNCTION fomp_get_max_threads()
 
     INTEGER :: fomp_get_max_threads
 
-
-
-
     fomp_get_max_threads = -1
-
 
 END FUNCTION fomp_get_max_threads
 
 
 FUNCTION fomp_get_thread_num()
-
-
-
 
     IMPLICIT NONE
 
@@ -85,19 +55,12 @@ FUNCTION fomp_get_thread_num()
 
     INTEGER :: fomp_get_thread_num
 
-
-
-
     fomp_get_thread_num = -1
-
 
 END FUNCTION fomp_get_thread_num
 
 
 FUNCTION fomp_get_num_procs()
-
-
-
 
     IMPLICIT NONE
 
@@ -105,19 +68,12 @@ FUNCTION fomp_get_num_procs()
 
     INTEGER :: fomp_get_num_procs
 
-
-
-
     fomp_get_num_procs = -1
-
 
 END FUNCTION fomp_get_num_procs
 
 
 FUNCTION fomp_in_parallel()
-
-
-
 
     IMPLICIT NONE
 
@@ -125,19 +81,12 @@ FUNCTION fomp_in_parallel()
 
     LOGICAL :: fomp_in_parallel
 
-
-
-
     fomp_in_parallel = .FALSE.
-
 
 END FUNCTION fomp_in_parallel
 
 
 SUBROUTINE fomp_set_dynamic(dynamic_threads)
-
-
-
 
     IMPLICIT NONE
 
@@ -145,17 +94,10 @@ SUBROUTINE fomp_set_dynamic(dynamic_threads)
 
     LOGICAL, INTENT(IN) :: dynamic_threads
 
-
-
-
-
 END SUBROUTINE fomp_set_dynamic
 
 
 FUNCTION fomp_get_dynamic()
-
-
-
 
     IMPLICIT NONE
 
@@ -163,19 +105,12 @@ FUNCTION fomp_get_dynamic()
 
     LOGICAL :: fomp_get_dynamic
 
-
-
-
     fomp_get_dynamic = .FALSE.
-
 
 END FUNCTION fomp_get_dynamic
 
 
 SUBROUTINE fomp_set_nested(nested)
-
-
-
 
     IMPLICIT NONE
 
@@ -183,17 +118,10 @@ SUBROUTINE fomp_set_nested(nested)
 
     LOGICAL, INTENT(IN) :: nested
 
-
-
-
-
 END SUBROUTINE fomp_set_nested
 
 
 FUNCTION fomp_get_nested()
-
-
-
 
     IMPLICIT NONE
 
@@ -201,18 +129,12 @@ FUNCTION fomp_get_nested()
 
     LOGICAL :: fomp_get_nested
 
-
-
-
     fomp_get_nested = .FALSE.
-
 
 END FUNCTION fomp_get_nested
 
 
 SUBROUTINE fomp_set_schedule(kind, modifier)
-
-
 
     USE omp_constants, ONLY : fomp_sched_kind
 
@@ -223,16 +145,10 @@ SUBROUTINE fomp_set_schedule(kind, modifier)
     INTEGER(KIND=fomp_sched_kind), INTENT(IN) :: kind
     INTEGER, INTENT(IN) :: modifier
 
-
-
-
-
 END SUBROUTINE fomp_set_schedule
 
 
 SUBROUTINE fomp_get_schedule(kind, modifier)
-
-
 
     USE omp_constants, ONLY : fomp_sched_kind
 
@@ -243,20 +159,13 @@ SUBROUTINE fomp_get_schedule(kind, modifier)
     INTEGER(KIND=fomp_sched_kind), INTENT(OUT) :: kind
     INTEGER, INTENT(OUT) :: modifier
 
-
-
-
     kind = -1
     modifier = -1
-
 
 END SUBROUTINE fomp_get_schedule
 
 
 FUNCTION fomp_get_thread_limit()
-
-
-
 
     IMPLICIT NONE
 
@@ -264,41 +173,23 @@ FUNCTION fomp_get_thread_limit()
 
     INTEGER :: fomp_get_thread_limit
 
-
-
-
     fomp_get_thread_limit = -1
-
 
 END FUNCTION fomp_get_thread_limit
 
 
 SUBROUTINE fomp_set_max_active_levels(max_levels)
 
-
-
-
     IMPLICIT NONE
 
     !f2py threadsafe
 
-
-
-
     INTEGER, INTENT(IN) :: max_levels
-
-
-
-
-
 
 END SUBROUTINE fomp_set_max_active_levels
 
 
 FUNCTION fomp_get_max_active_levels()
-
-
-
 
     IMPLICIT NONE
 
@@ -306,19 +197,12 @@ FUNCTION fomp_get_max_active_levels()
 
     INTEGER :: fomp_get_max_active_levels
 
-
-
-
     fomp_get_max_active_levels = -1
-
 
 END FUNCTION fomp_get_max_active_levels
 
 
 FUNCTION fomp_get_level()
-
-
-
 
     IMPLICIT NONE
 
@@ -326,19 +210,12 @@ FUNCTION fomp_get_level()
 
     INTEGER :: fomp_get_level
 
-
-
-
     fomp_get_level = -1
-
 
 END FUNCTION fomp_get_level
 
 
 FUNCTION fomp_get_ancestor_thread_num(level)
-
-
-
 
     IMPLICIT NONE
 
@@ -347,19 +224,12 @@ FUNCTION fomp_get_ancestor_thread_num(level)
     INTEGER, INTENT(IN) :: level
     INTEGER :: fomp_get_ancestor_thread_num
 
-
-
-
     fomp_get_ancestor_thread_num = -1
-
 
 END FUNCTION fomp_get_ancestor_thread_num
 
 
 FUNCTION fomp_get_team_size(level)
-
-
-
 
     IMPLICIT NONE
 
@@ -368,19 +238,12 @@ FUNCTION fomp_get_team_size(level)
     INTEGER, INTENT(IN) :: level
     INTEGER :: fomp_get_team_size
 
-
-
-
     fomp_get_team_size = -1
-
 
 END FUNCTION fomp_get_team_size
 
 
 FUNCTION fomp_get_active_level()
-
-
-
 
     IMPLICIT NONE
 
@@ -388,19 +251,12 @@ FUNCTION fomp_get_active_level()
 
     INTEGER :: fomp_get_active_level
 
-
-
-
     fomp_get_active_level = -1
-
 
 END FUNCTION fomp_get_active_level
 
 
 FUNCTION fomp_in_final()
-
-
-
 
     IMPLICIT NONE
 
@@ -408,18 +264,12 @@ FUNCTION fomp_in_final()
 
     LOGICAL :: fomp_in_final
 
-
-
-
     fomp_in_final = .FALSE.
-
 
 END FUNCTION fomp_in_final
 
 
 SUBROUTINE fomp_init_lock(svar)
-
-
 
     USE omp_constants, ONLY : fomp_lock_kind
 
@@ -429,18 +279,12 @@ SUBROUTINE fomp_init_lock(svar)
 
     INTEGER(KIND=fomp_lock_kind), INTENT(OUT) :: svar
 
-
-
-
     svar = -1
-
 
 END SUBROUTINE fomp_init_lock
 
 
 SUBROUTINE fomp_init_nest_lock(nvar)
-
-
 
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
@@ -450,19 +294,13 @@ SUBROUTINE fomp_init_nest_lock(nvar)
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(OUT) :: nvar
 
-
-
-
     nvar = -1
-
 
 END SUBROUTINE fomp_init_nest_lock
 
 
 SUBROUTINE fomp_destroy_lock(svar)
 
-
-
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
@@ -470,18 +308,12 @@ SUBROUTINE fomp_destroy_lock(svar)
     !f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
-
-
-
-
 
 END SUBROUTINE fomp_destroy_lock
 
 
 SUBROUTINE fomp_destroy_nest_lock(nvar)
 
-
-
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
@@ -489,18 +321,12 @@ SUBROUTINE fomp_destroy_nest_lock(nvar)
     !f2py threadsafe
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
-
-
-
-
 
 END SUBROUTINE fomp_destroy_nest_lock
 
 
 SUBROUTINE fomp_set_lock(svar)
 
-
-
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
@@ -508,18 +334,12 @@ SUBROUTINE fomp_set_lock(svar)
     !f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
-
-
-
-
 
 END SUBROUTINE fomp_set_lock
 
 
 SUBROUTINE fomp_set_nest_lock(nvar)
 
-
-
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
@@ -528,16 +348,10 @@ SUBROUTINE fomp_set_nest_lock(nvar)
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
 
-
-
-
-
 END SUBROUTINE fomp_set_nest_lock
 
 
 SUBROUTINE fomp_unset_lock(svar)
-
-
 
     USE omp_constants, ONLY : fomp_lock_kind
 
@@ -547,16 +361,10 @@ SUBROUTINE fomp_unset_lock(svar)
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
 
-
-
-
-
 END SUBROUTINE fomp_unset_lock
 
 
 SUBROUTINE fomp_unset_nest_lock(nvar)
-
-
 
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
@@ -566,16 +374,10 @@ SUBROUTINE fomp_unset_nest_lock(nvar)
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
 
-
-
-
-
 END SUBROUTINE fomp_unset_nest_lock
 
 
 FUNCTION fomp_test_lock(svar)
-
-
 
     USE omp_constants, ONLY : fomp_lock_kind
 
@@ -586,20 +388,12 @@ FUNCTION fomp_test_lock(svar)
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
     LOGICAL :: fomp_test_lock
 
-
-
-
     fomp_test_lock = .FALSE.
-
-
-
 
 END FUNCTION fomp_test_lock
 
 
 FUNCTION fomp_test_nest_lock(nvar)
-
-
 
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
@@ -610,21 +404,12 @@ FUNCTION fomp_test_nest_lock(nvar)
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
     INTEGER :: fomp_test_nest_lock
 
-
-
-
     fomp_test_nest_lock = -1
-
-
-
 
 END FUNCTION fomp_test_nest_lock
 
 
 FUNCTION fomp_get_wtime()
-
-
-
 
     IMPLICIT NONE
 
@@ -632,21 +417,12 @@ FUNCTION fomp_get_wtime()
 
     REAL (KIND=8) :: fomp_get_wtime
 
-
-
-
     fomp_get_wtime = -1
-
-
-
 
 END FUNCTION fomp_get_wtime
 
 
 FUNCTION fomp_get_wtick()
-
-
-
 
     IMPLICIT NONE
 
@@ -654,13 +430,7 @@ FUNCTION fomp_get_wtick()
 
     REAL (KIND=8) :: fomp_get_wtick
 
-
-
-
     fomp_get_wtick = -1
-
-
-
 
 END FUNCTION fomp_get_wtick
 
