014be8d49e154f881602d2d78f9e34853dc0c8d6
Bill Ladwig <ladwig@ucar.edu>
2018-01-23 10:53:12 -0700

    Updated build scripts.

--- build_scripts/gnu_no_omp.sh
+++ build_scripts/gnu_no_omp.sh
@@ -5,16 +5,6 @@ gfortran -E ompgen.F90 -cpp -o omp.f90
 f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature
 cd ..
 
-
-if [$CONDA_BUILD == 1]
-	if [ `uname` == Darwin ]; then
-    	LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
-	fi
-	$PYTHON setup.py clean --all
-	$PYTHON setup.py config_fc --f90flags="-mtune=generic" build_ext build
-	$PYTHON setup.py install --single-version-externally-managed --record=record.txt
-else
-	python setup.py clean --all
-	python setup.py config_fc --f90flags="-mtune=generic" build_ext build
-	pip install .
-fi
+python setup.py clean --all
+python setup.py config_fc --f90flags="-mtune=generic" build_ext build
+pip install .
--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cd build_scripts
-sh gnu_omp.sh
+cd ./fortran
+gfortran -E ompgen.F90 -fopenmp -cpp -o omp.f90
+f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature
+cd ..
+
+if [ `uname` == Darwin ]; then
+    LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
+fi
+
+$PYTHON setup.py config_fc --f90flags="-mtune=generic -fopenmp" build_ext --libraries="gomp" build
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
 
