44ea4c9b703d1aa7986ed2cdd855d81087177a32
Bill Ladwig <ladwig@ucar.edu>
2018-01-26 15:57:54 -0700

    Added script for building on cheyenne

---
+++ build_scripts/chey_intel_mkl_omp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../fortran/build_help
+ifort -o sizes -qopenmp omp_sizes.f90
+python sub_sizes.py
+
+cd ..
+ifort ompgen.F90 -qopenmp -fpp -save-temps
+mv ompgen.i90 omp.f90
+f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature
+cd ..
+
+python setup.py clean --all
+python setup.py config_fc --f90flags="-O3 -xHost -mkl -openmp" build --compiler=intelem --fcompiler=intelem
+pip install .
+
