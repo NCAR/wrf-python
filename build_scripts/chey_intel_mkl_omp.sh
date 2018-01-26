7d53bb7837787862c48238083828fb9eaab64fab
Bill Ladwig <ladwig@ucar.edu>
2018-01-26 16:29:19 -0700

    Fixed some typos. Build still needs paths to MKL and OMP in order to run

--- build_scripts/chey_intel_mkl_omp.sh
+++ build_scripts/chey_intel_mkl_omp.sh
@@ -5,12 +5,12 @@ ifort -o sizes -qopenmp omp_sizes.f90
 python sub_sizes.py
 
 cd ..
-ifort ompgen.F90 -qopenmp -fpp -save-temps
+ifort ompgen.F90 -qopenmp -fpp -save-temps 2>/dev/null
 mv ompgen.i90 omp.f90
 f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature
 cd ..
 
 python setup.py clean --all
-python setup.py config_fc --f90flags="-O3 -xHost -mkl -openmp" build --compiler=intelem --fcompiler=intelem
+python setup.py config_fc --f90flags="-O3 -xHost -mkl -qopenmp" build --compiler=intelem --fcompiler=intelem
 pip install .
 
