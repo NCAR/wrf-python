0b02c0a952c1dc24b16761649f4adae7e22949ab
Bill Ladwig <ladwig@ucar.edu>
2018-02-01 11:02:39 -0700

    Fix typo

--- build_scripts/chey_intel_mkl_omp.sh
+++ build_scripts/chey_intel_mkl_omp.sh
@@ -11,7 +11,7 @@ f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature
 cd ..
 
 python setup.py clean --all
-python setup.py config_fc --f90flags="-O3 -xHost -mkl -openmp" build_ext --rpath="/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/lib/intel64_lin:/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/mkl/lib/intel64_lin" --library-dirs="/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/lib/intel64_lin:/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/mkl/lib/intel64_lin" --libraries="mkl_intel_ilp64 mkl_intel_thread mkl_core iomp5 pthread m dl" build --compiler=intelem --fcompiler=intelem
+python setup.py config_fc --f90flags="-O3 -xHost -mkl -qopenmp" build_ext --rpath="/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/lib/intel64_lin:/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/mkl/lib/intel64_lin" --library-dirs="/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/lib/intel64_lin:/glade/u/apps/opt/intel/2017u1/compilers_and_libraries/linux/mkl/lib/intel64_lin" --libraries="mkl_intel_ilp64 mkl_intel_thread mkl_core iomp5 pthread m dl" build --compiler=intelem --fcompiler=intelem
 
 #pip install .
 
