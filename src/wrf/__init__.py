b9dbdd3e1a1661a81c1005be0d869a1d7e766385
Katelyn FitzGerald <7872563+kafitzgerald@users.noreply.github.com>
2025-07-17 10:18:28 -0600

    remove deprecated use of pkg_resources

--- src/wrf/__init__.py
+++ src/wrf/__init__.py
@@ -2,24 +2,8 @@ from __future__ import (absolute_import, division, print_function)
 import os
 import pkg_resources
 
-try:
-    from . import api
-    from .api import *
-except ImportError:
-    # For gfortran+msvc combination, extra shared libraries may exist
-    # (stored by numpy.distutils)
-    if os.name == "nt":
-        req = pkg_resources.Requirement.parse("wrf-python")
-        extra_dll_dir = pkg_resources.resource_filename(req,
-                                                        "wrf-python/.libs")
-        if os.path.isdir(extra_dll_dir):
-            os.environ["PATH"] += os.pathsep + extra_dll_dir
-
-        from . import api
-        from .api import *
-    else:
-        raise
-
+from . import api
+from .api import *
 
 __all__ = []
 __all__.extend(api.__all__)
